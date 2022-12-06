397f7e3da81aa4ccc5b7ddd22267ede323515843
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2022-12-06 10:33:28 +0300

    Create onPageLoad.js

---
+++ libs/onPageLoad.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        DTF function runner
+// @namespace   https://github.com/TentacleTenticals
+// @match       https://dtf.ru/*
+// @grant       none
+// @version     1.1
+// @author      Tentacle Tenticals
+// @description Лоадер функций
+// @homepage https://github.com/TentacleTenticals/
+// @license MIT
+// ==/UserScript==
+/* jshint esversion:8 */
+
+(function() {
+'use strict';
+  window.onPageLoad = (run) => {
+    {
+    const log = console.log.bind(console)
+    console.log = (...args) => {
+      if(Array.isArray(args)){
+        if(args[0]){
+          if(typeof args[0] === 'string'){
+            if(args[0].match(/\[ Air \] Ready.*/)){
+              run();
+            }
+          }
+        }
+      }
+      log(...args);
+    }}
+  }
+}();
