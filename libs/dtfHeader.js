b3393a29e625c40bc1499fe0a9116cc387a77312
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2022-12-06 10:51:37 +0300

    Create dtfHeader.js

---
+++ libs/dtfHeader.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        DTF header
+// @namespace   https://github.com/TentacleTenticals
+// @match       https://dtf.ru/*
+// @grant       none
+// @version     1.1
+// @author      Tentacle Tenticals
+// @description Хеадер статей DTF
+// @homepage https://github.com/TentacleTenticals/
+// @license MIT
+// ==/UserScript==
+/* jshint esversion:8 */
+
+(function() {
+'use strict';
+  class DtfHeader(){
+    constructor(path, where){
+      if(document.getElementById('Dtf-header')) return;
+      this.main=document.createElement('div');
+      this.main.className='Dtf-header';
+      this.main.id='Dtf-header';
+      this.main.style=`
+      width: 100%;
+      `;
+      path.parentNode.insertBefore(this.main, where);
+    }
+  }
+}();
