bdbdd80a0e15ca19b00a6320b6bbc69519afa6d6
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-08 14:38:04 +0300

    Create db.js

---
+++ emoji/db.js
@@ -0,0 +1,16 @@
+let emoji = {
+  anime: {
+    kawai: {type:'na', url:'https://i.imgur.com/kXIjx5C.jpg'},
+    neko: {type:'na', url:'https://i.imgur.com/I3mRoT0.jpg'},
+    desu: {type:'na', url:'https://i.imgur.com/jEQRFMJ.jpg'},
+    shy: {type:'a', url:'https://i.imgur.com/kzfjOlt.gifv'},
+    cultist: {type:'a', url:'https://i.imgur.com/7AF6Xk1.mp4'}
+  },
+  rangers: {
+    maloc: {type:'na', url:'https://i.imgur.com/ymAqNjU.png'},
+    fay: {type:'na', url:'https://i.imgur.com/IfW6L3a.png'},
+    pirate: {type:'na', url:'https://i.imgur.com/Sy2kHwb.png'},
+    teo: {type:'na', url:'https://i.imgur.com/s312kKi.png'},
+    fox: {type:'na', url:'https://i.imgur.com/dXUUSzi.png'}
+  }
+}
