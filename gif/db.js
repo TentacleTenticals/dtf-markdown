c669043e8e285c9fb8c1d90ff372f6485b00cff2
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-08 14:41:33 +0300

    Create db.js

---
+++ gif/db.js
@@ -0,0 +1,40 @@
+let gifs = {
+  'dtf': {
+    'porv': {
+      name: 'porv',
+      group: 'dtf',
+      id: 'dtf:porv',
+      gifId: 'NecessaryShimmeringKingsnake'
+    },
+    'porv1': {
+      name: 'porv1',
+      group: 'dtf',
+      id: 'dtf:porv1',
+      gifId: 'WaterloggedKindBlackandtancoonhound'
+    },
+    'porv2': {
+      name: 'porv2',
+      group: 'dtf',
+      id: 'dtf:porv2',
+      gifId: 'EnragedCookedDesertpupfish'
+    }
+  },
+
+  'asian': {
+    'heh1': {
+      name: 'heh1',
+      group: 'asian',
+      id: 'asian:heh1',
+      gifId: 'CraftyGrotesqueDanishswedishfarmdog'
+    }
+  },
+
+  'other': {
+    'laugh1': {
+      name: 'laugh1',
+      group: 'other',
+      id: 'other:laugh1',
+      gifId: 'YellowGrayInganue'
+    }
+  }
+};
