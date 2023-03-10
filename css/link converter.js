fd7e2113881f20f3d6a85648b1eef56ed0618ec9
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-10 18:33:21 +0300

    Create link converter.js

---
+++ css/link converter.js
@@ -0,0 +1,35 @@
+let linkConverter = `
+.linkConverter form {
+  display: flex;
+  flex-direction: row;
+  gap: 5px 5px;
+  margin: 0 0 5px 0;
+}
+
+.linkConverter .mask {
+  display: flex;
+  width: 150px;
+  height: 150px;
+  margin: auto;
+  padding: 3px;
+  border-radius: 2px;
+  box-shadow: 0px 0px 2px 0px rgb(255 255 255);
+}
+
+.linkConverter video {
+  max-width: 150px;
+  max-height: 150px;
+  margin: auto;
+}
+.linkConverter .image {
+  max-width: 150px;
+  max-height: 150px;
+}
+.linkConverter .sticker {
+  max-width: 120px;
+  max-height: 120px;
+}
+.linkConverter .emoji {
+  max-width: 30px;
+  max-height: 30px;
+}`;
