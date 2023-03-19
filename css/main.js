f52bf6da1a7037f9e6ddad6821c2918a0b820546
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-19 20:42:16 +0300

    Update and rename main css.js to main.js

--- css/main.js
+++ css/main.js
@@ -1,4 +1,4 @@
-let mainCss = `
+let mainCSS = `
 @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Play&display=swap');
 
@@ -7,6 +7,19 @@ let mainCss = `
 @import url('https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@0,400;1,400&display=swap');
 
 @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@800&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Golos+Text:wght@500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Sofia+Sans+Semi+Condensed:wght@400;500;600;700&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@400;500;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Philosopher:wght@400;700&display=swap');
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Flex:wght@400;500;600;700&display=swap');
 
 .comment-form {
   display: flex;
@@ -18,21 +31,150 @@ let mainCss = `
   min-height: 100px;
 }
 
+.container.px label::before {
+  display: inline;
+  content: 'px';
+  color: rgb(167 167 167);
+  margin: 0 4px 0 0;
+  font-size: 10px;
+}
+
+.commentsPreviewer {
+  min-width: 300px;
+  color: rgb(255 255 255);
+}
+
+.dtf-scriptWindow input {
+  outline: none;
+}
+
+.dtf-scriptWindow input[type="checkbox"] {
+  padding: unset;
+  margin: unset;
+}
+
+.dtf-scriptWindow input[type="color"] {
+  aspect-ratio: 1/1;
+  cursor: pointer;
+}
+
+.dtf-scriptWindow form {
+  display: flex;
+  flex-direction: column;
+  gap: 6px 0;
+}
+
+.dtf-scriptWindow legend {
+  width: max-content;
+  font-weight: 500;
+  font-size: 17px;
+  font-family: 'Roboto Flex', sans-serif;
+  font-family: 'Philosopher', sans-serif;
+  font-family: 'Golos Text', sans-serif;
+  font-family: 'Inter', sans-serif;
+  font-family: 'Nunito Sans', sans-serif;
+  font-family: 'Mulish', sans-serif;
+  font-family: 'Sofia Sans Semi Condensed', sans-serif;
+  font-weight: 600;
+}
+.dtf-scriptWindow .info {
+  padding: 5px;
+  font-size: 12px;
+  font-family: 'Roboto Flex', sans-serif;
+  font-family: 'Philosopher', sans-serif;
+  font-family: 'Golos Text', sans-serif;
+  font-family: 'Inter', sans-serif;
+  font-family: 'Nunito Sans', sans-serif;
+  font-family: 'Mulish', sans-serif;
+  font-family: 'Sofia Sans Semi Condensed', sans-serif;
+  font-weight: 500;
+  border-radius: 2px;
+  box-shadow: inset 0 0 3px 1px rgb(157 157 157);
+}
+.dtf-scriptWindow .input-label {
+  font-size: 13px;
+  position: relative;
+  top: -2px;
+  font-family: 'Golos Text', sans-serif;
+  font-size: 13px;
+  font-family: 'Inter', sans-serif;
+  font-family: 'Nunito Sans', sans-serif;
+  font-family: 'Mulish', sans-serif;
+  font-family: 'Sofia Sans Semi Condensed', sans-serif;
+  font-family: 'Philosopher', sans-serif;
+  font-family: 'Roboto Flex', sans-serif;
+  font-weight: 500;
+}
+.dtf-scriptWindow .container :is(label) {
+  font-size: 13px;
+  position: relative;
+  top: -2px;
+  font-family: 'Golos Text', sans-serif;
+  font-size: 13px;
+  font-family: 'Inter', sans-serif;
+  font-family: 'Nunito Sans', sans-serif;
+  font-family: 'Mulish', sans-serif;
+  font-family: 'Sofia Sans Semi Condensed', sans-serif;
+  font-family: 'Philosopher', sans-serif;
+  font-family: 'Roboto Flex', sans-serif;
+  font-weight: 500;
+}
+.dtf-scriptWindow .container {
+  display: flex;
+  gap: 0 5px;
+}
+
+.dtf-scriptWindow fieldset.flex {
+  display: flex;
+  flex-direction: column;
+}
+.dtf-scriptWindow fieldset.grid .list {
+  display: grid;
+  grid-template-columns: repeat(2, max-content);
+}
+
+.dtf-scriptWindow ul.discordText {
+  display: flex;
+  flex-direction: column;
+  border-left: 4px solid rgb(185 59 59);
+  border-radius: 3px;
+  padding: 0 0 0 10px;
+  margin: 0 0 0 2px;
+  font-size: 13px;
+  font-weight: 400;
+  font-family: 'Mulish', sans-serif;
+  gap: 8px 0;
+  box-shadow: 0 0 3px 0px rgb(0 0 0);
+}
+.dtf-scriptWindow ul.discordText.alert {
+  background-color: rgb(249 207 207);
+  color: rgb(0 0 0);
+  font-weight: 700;
+}
+.dtf-scriptWindow ul.discordText.info {
+  background-color: rgb(139 215 172);
+  color: rgb(0 0 0);
+  font-weight: 700;
+}
+.dtf-scriptWindow li {
+  white-space: pre-line;
+}
+.dtf-scriptWindow ul.discordText li::marker {
+  font-size: 0;
+}
+
 .dtf-albumBuilder.container {
   display: flex;
   flex-direction: column;
 }
 
-.dtf-commentText {
-  display: inline;
-}
-
 .dtf-markdownPanel {
   width: 100%;
   position: relative;
-  background: black;
+  background-color: black;
+  padding: 2px 1px 2px 1px;
   display: flex;
-  gap: 0 0px;
+  gap: 0 0;
 }
 .dtf-markdownPanel .button {
   background-color: rgb(37 54 72);
@@ -238,4 +380,4 @@ html .ui-autocomplete {
   position: relative;
   box-shadow: 0px 0px 1px 1px rgb(185 0 87);
 }
-`
+`;
