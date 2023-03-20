be16cd2c9073257224b45f72eac8a6205058060a
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-20 20:00:08 +0300

    Update converter.js

--- link/converter.js
+++ link/converter.js
@@ -1,6 +1,28 @@
 class LinkConverter{
+  Selection(){
+    if(!window.getSelection().focusNode.isContentEditable && !window.getSelection().focusNode.parentNode.isContentEditable) return console.log('Wrong element', window.getSelection().focusNode);
+    this.sel = {
+      s: window.getSelection().focusNode.textContent.substring(0, window.getSelection().anchorOffset),
+      t: window.getSelection().focusNode.textContent.substring(window.getSelection().anchorOffset, window.getSelection().focusOffset),
+      e: window.getSelection().focusNode.textContent.substring(window.getSelection().focusOffset, window.getSelection().focusNode.textContent.length),
+      target: window.getSelection().focusNode,
+      offset: window.getSelection().anchorOffset
+    }
+  }
+  Modify(s){
+    this.Selection();
+    if(!this.sel) return;
+
+    if(this.sel.target.textContent.length === this.sel.offset){
+      this.sel.target.textContent = `${this.sel.target.textContent}${s}`;
+    }else{
+      this.sel.target.textContent = `${this.sel.s}${s}${this.sel.t}${this.sel.e}`;
+    }
+    if(mainCfg['link converter']['close after pick']) this.main.remove();
+  }
   constructor(path){
     if(document.getElementById('dtf-linkConverter')) return;
+    this.Selection();
     this.main=new Div({
       path: path,
       cName: 'dtf-window linkConverter',
@@ -16,7 +38,7 @@ class LinkConverter{
         this.main.remove();
       }
     });
-    this.headerLabel=new Div({
+    new Div({
       path: this.header,
       cName: 'label',
       text: 'LINK CONVERTER'
@@ -38,80 +60,97 @@ class LinkConverter{
       pattern: 'http(s):\/\/.+',
       rtn: [],
       onchange: (e) => {
-        setTimeout(() => {
-          e.target.setAttribute('added', e.target.value);
-        }, 100);
+        // if(e.target.getAttribute('added')) e.target.removeAttribute('added');
+        // setTimeout(() => {
+        //   e.target.setAttribute('added', e.target.value);
+        // }, 100);
         this.preview.className=this.type.value;
 
-        if(this.type.value.match(/gif|emojiGif/)){
+        if(this.type.value.match(/^(gif|emojiGif)$/)){
           this.preview.src=e.target.value;
           this.preview.poster='';
-        }else{
+        }else
+        if(this.type.value.match(/^(emoji|sticker|image)$/)){
           this.preview.poster=e.target.value;
           this.preview.src='';
         }
-      },
-      onkeyup: (e) => {
-        if(!e.target.value) return;
-        if(e.target.value.match(/http(s):\/\/.+/) && e.target.getAttribute('added') === e.target.value && e.code === 'Enter'){
-          document.querySelector(`.content_editable`).textContent += (() => {
-            switch(this.type.value){
-              case 'gif': return `:g:${urlCoder.decoder(e.target.value)}:g:`;
-              case 'image': `:i:${urlCoder.decoder(e.target.value)}:i:`;
-              case 'emoji': return `:e:${urlCoder.decoder(e.target.value)}:e:`;
-              case 'sticker': return `:s:${urlCoder.decoder(e.target.value)}:s:`;
-              case 'emojiGif': return `:eg:${urlCoder.decoder(e.target.value)}:eg:`;
-              case 'stickerGif': return `:sg:${urlCoder.decoder(e.target.value)}:sg:`;
-              case 'gifUrl': return e.target.value;
-              case 'imageUrl': return e.target.value;
-            }
-          })();
-        }
+      }
+    })
+
+    new Button({
+      path: this.form,
+      text: 'Ok',
+      onclick: () => {
+        if(!this.input.value) return;
+        let res = (() => {
+          switch(this.type.value){
+            case 'gif': return `:g:${urlCoder.decoder(this.input.value)}:g:`;
+            case 'image': `:i:${urlCoder.decoder(this.input.value)}:i:`;
+            case 'emoji': return `:e:${urlCoder.decoder(this.input.value)}:e:`;
+            case 'sticker': return `:s:${urlCoder.decoder(this.input.value)}:s:`;
+            case 'emojiGif': return `:eg:${urlCoder.decoder(this.input.value)}:eg:`;
+            case 'stickerGif': return `:sg:${urlCoder.decoder(this.input.value)}:sg:`;
+            case 'embed': return `:emb:${urlCoder.decoder(this.input.value)}:emb:`;
+            case 'link': return `<a>${urlCoder.decoder(this.input.value)}<a>`;
+            case 'url': return urlCoder.decoder(this.input.value);
+          }
+        })();
+        this.Modify(res);
       }
     })
 
     this.type=new Select({
       path: this.form,
       name: 'linkMode',
-      value: 'gif',
+      value: mainCfg['link converter']['default mode'],
       rtn: [],
-      onchange: (e) => {
-        console.log(this.input)
-        this.preview.className=e.target.value;
-        if(e.target.value.match(/gif|emojiGif/)){
-          this.preview.src=this.input.value;
-          this.preview.poster='';
-        }else{
-          this.preview.poster=this.input.value;
-          this.preview.src='';
-        }
-      },
-      body: (path, Optgroup) => {
-        this.gifs=new Optgroup({
-          path: path,
-          label: 'Gifs',
+      optgroups: [
+        {
+          label: 'GIFs',
           option: 'gif'
-        });
-        this.images=new Optgroup({
-          path: path,
+        },
+        {
           label: 'Images',
           option: 'image'
-        });
-        this.emojis=new Optgroup({
-          path: path,
+        },
+        {
           label: 'Emojis',
           options: ['emoji', 'emojiGif']
-        });
-        this.stickers=new Optgroup({
-          path: path,
+        },
+        {
           label: 'Stickers',
           options: ['sticker', 'stickerGif']
-        });
-        this.urls=new Optgroup({
-          path: path,
+        },
+        {
+          label: 'Embeds',
+          option: 'embed'
+        },
+        {
+          label: 'Links',
+          option: 'link'
+        },
+        {
           label: 'Urls',
-          options: ['gifUrl', 'imageUrl']
-        });
+          option: 'url'
+        }
+      ],
+      onchange: (e) => {
+        this.preview.className=e.target.value;
+        if(this.type.value.match(/^(gif|emojiGif)$/)){
+          this.preview.src=e.target.value;
+          this.preview.poster='';
+        }else
+        if(this.type.value.match(/^(emoji|sticker|image)$/)){
+          this.preview.poster=e.target.value;
+          this.preview.src='';
+        }
+        // if(e.target.value.match(/gif|emojiGif/)){
+        //   this.preview.src=this.input.value;
+        //   this.preview.poster='';
+        // }else{
+        //   this.preview.poster=this.input.value;
+        //   this.preview.src='';
+        // }
       }
     });
 
@@ -121,10 +160,10 @@ class LinkConverter{
       rtn: []
     });
 
-    this.preview=document.createElement('video');
-    this.preview.className='preview';
-    this.preview.src='';
-    this.preview.poster='';
-    this.mask.appendChild(this.preview);
+    this.preview=new Video({
+      path: this.mask,
+      cName: 'preview',
+      rtn: []
+    });
   }
 }
