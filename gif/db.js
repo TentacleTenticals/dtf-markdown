787db413e4b4fae3d715e332edb14f5db56c34d2
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-22 15:18:31 +0300

    Update db.js

--- gif/db.js
+++ gif/db.js
@@ -38,3 +38,60 @@ let gifsDB = {
     }
   }
 };
+// export let gifs = [
+//   {
+//     name: 'porv',
+//     group: 'dtf',
+//     id: 'dtf:porv',
+//     gifId: 'NecessaryShimmeringKingsnake',
+//     url: 'https://i.imgur.com/UirqpgY.mp4'
+//   },
+//   {
+//     name: 'porv1',
+//     group: 'dtf',
+//     id: 'dtf:porv1',
+//     gifId: 'WaterloggedKindBlackandtancoonhound',
+//     url: 'https://i.imgur.com/UhW3r5L.mp4'
+//   },
+//   {
+//     name: 'porv2',
+//     group: 'dtf',
+//     id: 'dtf:porv2',
+//     gifId: 'EnragedCookedDesertpupfish',
+//     url: ''
+//   },
+
+//   {
+//     name: 'heh1',
+//     group: 'asian',
+//     id: 'asian:heh1',
+//     gifId: 'CraftyGrotesqueDanishswedishfarmdog',
+//     url: ''
+//   },
+//   {
+//     name: 'laugh1',
+//     group: 'other',
+//     id: 'other:laugh1',
+//     gifId: 'YellowGrayInganue',
+//     url: ''
+//   }
+// ];
+// export let gifs = {
+//   'dtf': [
+//     {
+//       name: 'porv',
+//       group: 'dtf',
+//       id: 'dtf:porv',
+//       gifId: 'NecessaryShimmeringKingsnake',
+//       url: 'https://i.imgur.com/UirqpgY.mp4'
+//     },
+//     {
+//       name: 'porv1',
+//       group: 'dtf',
+//       id: 'dtf:porv1',
+//       gifId: 'WaterloggedKindBlackandtancoonhound',
+//       url: 'https://i.imgur.com/UhW3r5L.mp4'
+//     }
+//   ]
+// }
+// https://gfycat.com/necessaryshimmeringkingsnake
