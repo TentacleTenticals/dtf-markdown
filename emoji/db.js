e26394e2ea75c0cc2f58c4ab7814e27642270390
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-22 15:15:39 +0300

    Update db.js

--- emoji/db.js
+++ emoji/db.js
@@ -1,10 +1,17 @@
-let emoji = {
+let emojisDB = {
   anime: {
-    kawai: {type:'na', url:'https://i.imgur.com/kXIjx5C.jpg'},
-    neko: {type:'na', url:'https://i.imgur.com/I3mRoT0.jpg'},
-    desu: {type:'na', url:'https://i.imgur.com/jEQRFMJ.jpg'},
-    shy: {type:'a', url:'https://i.imgur.com/kzfjOlt.gifv'},
-    cultist: {type:'a', url:'https://i.imgur.com/7AF6Xk1.mp4'}
+    // kawai: {type:'na', url:'https://i.imgur.com/kXIjx5C.jpg'},
+    // neko: {type:'na', url:'https://i.imgur.com/I3mRoT0.jpg'},
+    // desu: {type:'na', url:'https://i.imgur.com/jEQRFMJ.jpg'},
+    // shy: {type:'a', url:'https://i.imgur.com/kzfjOlt.gifv'},
+    // cultist: {type:'a', url:'https://i.imgur.com/7AF6Xk1.mp4'},
+    'cool': {type: 'na', url:'https://i.imgur.com/XacLspG.png'},
+    'smug': {type:'na', url:'https://i.imgur.com/aRfyEQX.png'},
+    'blush2b': {type:'na', url:'https://i.imgur.com/wuCb7HY.png'},
+    'meguwat': {type:'na', url:'https://i.imgur.com/H053Dmt.png'},
+    'ex1': {type:'na', url:'https://i.imgur.com/aE2H88U.png'},
+    'gonnakillya': {type:'na', url:'https://i.imgur.com/NLQ7nS6.png'},
+    'yes': {type:'na', url:'https://i.imgur.com/m9M8ENb.png'}
   },
   rangers: {
     maloc: {type:'na', url:'https://i.imgur.com/ymAqNjU.png'},
@@ -12,5 +19,8 @@ let emoji = {
     pirate: {type:'na', url:'https://i.imgur.com/Sy2kHwb.png'},
     teo: {type:'na', url:'https://i.imgur.com/s312kKi.png'},
     fox: {type:'na', url:'https://i.imgur.com/dXUUSzi.png'}
+  },
+  others: {
+    facepalm: {type:'na', url:'https://i.imgur.com/h8LsrAU.png'}
   }
 }
