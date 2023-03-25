167b985d5265c40d24b005c696bc4d20d1d6cb09
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-25 15:59:17 +0300

    Update album.js

--- album/album.js
+++ album/album.js
@@ -1,4 +1,108 @@
 class Album {
+  AlbumItem({path, url, text, spoiler}){
+  let mask=new Div({
+    path: path,
+    cName: `mask ${spoiler ? ' spoiler' : ''}`,
+    rtn: [],
+    tab: -1,
+    // onclick: (e) => {
+    //   e.target.focus();
+    // },
+    onfocus: (e) => {
+      e.target.classList.add('picked');
+      mainVars.picked = e.target;
+      new AlbumMiniPreviewer({
+        path: document.body
+      });
+    },
+    onkeydown: (e) => {
+      if(!mainVars.picked) return;
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      if(e.code === 'ArrowLeft'){
+        prevAlbumItem(e);
+      }else
+      if(e.code === 'ArrowRight'){
+        nextAlbumItem(e);
+      }else
+      if(e.code === 'Escape'){
+        mainVars.picked.blur();
+        mainVars.picked.children[1].style.scale = '1';
+        mainVars.picked.classList.remove('picked');
+        mainVars.picked.classList.remove('zoomed');
+        document.getElementById('dtf-previewer').remove();
+      }else
+      if(e.code === 'ControlLeft'){
+        mainVars.btnPressed.ctrl = true;
+      }
+    },
+    onkeyup: (e) => {
+      if(!mainVars.picked) return;
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      if(e.code === 'ControlLeft'){
+        mainVars.btnPressed.ctrl = false;
+      }
+    },
+    onwheel: (e) => {
+      // if(!e.target.className.match('picked')){
+      //   if(e.deltaY > 10 && e.target.nextSibling){
+      //     // alert('Yo')
+      //   e.preventDefault();
+      //   e.target.nextElementSibling.scrollIntoView();
+      //   }
+      // }
+      if(!mainVars.btnPressed.ctrl) return;
+      if(!mainVars.picked) return;
+      e.preventDefault();
+      e.stopPropagation();
+      e.stopImmediatePropagation();
+      console.log('Zoom mode on', e);
+      if(!mainVars.picked.classList.value.match(/zoomed/)){
+        mainVars.picked.classList.add('zoomed');
+      }else
+      {
+        if(e.deltaY > 0) zoom('out')
+        else
+        if(e.deltaY < 0) zoom('in');
+      }
+    }
+  });
+
+  this.buttonPanel=new Div({
+    path: mask,
+    cName: 'buttonPanel',
+    rtn: []
+  });
+  this.btnCopyLink=new Button({
+    path: this.buttonPanel,
+    cName: 'maskButton copyLink',
+    label: 'L'
+  });
+  this.btnCopyImg=new Button({
+    path: this.buttonPanel,
+    cName: 'maskButton copyImg',
+    label: 'I'
+  });
+
+  if(text){
+    this.hasText=new Div({
+      path: this.buttonPanel,
+      cName: 'hasText',
+      text: 'T'
+    });
+  }
+
+  this.img=new Image({
+    path: mask,
+    cName: 'img',
+    url: url,
+    text: text,
+    scale: '1'
+  });
+}
   constructor({path, type, albumArr, rtn}){
     this.main=new Div({
       path: path,
@@ -32,124 +136,15 @@ class Album {
 
 
     if(albumArr) for(let i = 0; i < albumArr.length; i++){
-      new AlbumItem({
+      this.AlbumItem({
         path: this.list,
         url: urlCoder.encoder(albumArr[i].u),
-        text: albumArr[i].t
+        text: albumArr[i].t,
+        spoiler: albumArr[i].s
       })
     }
 
     if(rtn === 'list') return this.list;
-  }
-}
-
-class AlbumItem {
-  constructor({path, url, text, spoiler}){
-    this.mask=new Div({
-      path: path,
-      cName: `mask${spoiler ? ' spoiler' : ''}`,
-      rtn: [],
-      tab: -1,
-      // onclick: (e) => {
-      //   e.target.focus();
-      // },
-      onfocus: (e) => {
-        e.target.classList.add('picked');
-        mainVars.picked = e.target;
-        new AlbumMiniPreviewer({
-          path: document.body
-        });
-      },
-      onkeydown: (e) => {
-        if(!mainVars.picked) return;
-        e.preventDefault();
-        e.stopPropagation();
-        e.stopImmediatePropagation();
-        if(e.code === 'ArrowLeft'){
-          prevAlbumItem(e);
-        }else
-        if(e.code === 'ArrowRight'){
-          nextAlbumItem(e);
-        }else
-        if(e.code === 'Escape'){
-          mainVars.picked.blur();
-          mainVars.picked.children[1].style.scale = '1';
-          mainVars.picked.classList.remove('picked');
-          mainVars.picked.classList.remove('zoomed');
-          document.getElementById('dtf-previewer').remove();
-        }else
-        if(e.code === 'ControlLeft'){
-          mainVars.btnPressed.ctrl = true;
-        }
-      },
-      onkeyup: (e) => {
-        if(!mainVars.picked) return;
-        e.preventDefault();
-        e.stopPropagation();
-        e.stopImmediatePropagation();
-        if(e.code === 'ControlLeft'){
-          mainVars.btnPressed.ctrl = false;
-        }
-      },
-      onwheel: (e) => {
-        // if(!e.target.className.match('picked')){
-        //   if(e.deltaY > 10 && e.target.nextSibling){
-        //     // alert('Yo')
-        //   e.preventDefault();
-        //   e.target.nextElementSibling.scrollIntoView();
-        //   }
-        // }
-        if(!mainVars.btnPressed.ctrl) return;
-        if(!mainVars.picked) return;
-        e.preventDefault();
-        e.stopPropagation();
-        e.stopImmediatePropagation();
-        console.log('Zoom mode on', e);
-        if(!mainVars.picked.classList.value.match(/zoomed/)){
-          mainVars.picked.classList.add('zoomed');
-        }else
-        {
-          if(e.deltaY > 0) zoom('out')
-          else
-          if(e.deltaY < 0) zoom('in');
-        }
-      }
-    });
-
-    this.buttonPanel=new Div({
-      path: this.mask,
-      cName: 'buttonPanel',
-      rtn: []
-    });
-    this.btnCopyLink=new Button({
-      path: this.buttonPanel,
-      cName: 'maskButton copyLink',
-      label: 'L'
-    });
-    this.btnCopyImg=new Button({
-      path: this.buttonPanel,
-      cName: 'maskButton copyImg',
-      label: 'I'
-    });
-
-    if(text){
-      this.hasText=new Div({
-        path: this.buttonPanel,
-        cName: 'hasText',
-        text: 'T'
-      });
-    }
-
-    this.img=new Image({
-      path: this.mask,
-      cName: 'img',
-      url: url,
-      text: text,
-      scale: '1',
-      onclick: () => {
-        console.log('Yo!');
-      }
-    });
   }
 }
 
