299b825966391ffa55c27d9424856101c04b1ee4
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2023-03-26 14:46:08 +0300

    Update album_builder.js

--- css/album_builder.js
+++ css/album_builder.js
@@ -1,16 +1,13 @@
 let albumBuilderCSS = `
 .dtf-albumBuilder .btn {
-  background-image: repeating-linear-gradient(
-    180deg,
-    rgb(90 90 90) 0%,
-    rgb(30 30 30) 50%,
-    rgb(90 90 90) 100%
-  );
-  background-color: rgb(0 0 0);
-  color: rgb(215 212 212);
-  margin: 0px auto 6px auto;
-  border-radius: 3px;
-  box-shadow: inset 0 0 2px 0px rgb(255 255 255);
+    background-image: repeating-linear-gradient(180deg, rgb(90 90 90) 0%, rgb(30 30 30) 50%, rgb(90 90 90) 100%);
+    background-color: rgb(0 0 0);
+    color: rgb(215 212 212);
+    margin: 0px auto 6px auto;
+    padding: 1px 3px 1px 3px;
+    font-size: 13px;
+    border-radius: 3px;
+    box-shadow: inset 0 0 2px 0px rgb(255 255 255);
 }
 .dtf-albumBuilder .btn:hover {
   filter: brightness(1.1);
